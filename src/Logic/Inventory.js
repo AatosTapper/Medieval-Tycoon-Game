f54f6e0cf7953e4a70e72fc08c90f0dd5e1bf8b9
AatosTapper <aatos.tapper@icloud.com>
2023-11-17 17:09:04 +0200

    TakeItemFromInventory function added (finally)

--- src/Logic/Inventory.js
+++ src/Logic/Inventory.js
@@ -14,4 +14,21 @@ const AddItemToInventory = (playerState, setPlayerState, itemName, amount) => {
     }));
 }
 
-export default AddItemToInventory;
+const TakeItemFromInventory = (playerState, setPlayerState, itemName, amount) => {
+    let id = null;
+    for (const key in ITEM) {
+        if (itemName === ITEM[key].name) {
+            id = newInventory[ITEM[key].id];
+            break;
+        }
+    }
+    if (id === null) {
+        console.log("Tried to get item that doens't exist from inventory.")
+        return;
+    }
+    const numItems = playerState.inventory[id];
+    amount = amount >= numItems ? numItems : amount;
+    AddItemToInventory(playerState, setPlayerState, itemName, -amount);
+}
+
+export default { AddItemToInventory, TakeItemFromInventory };
