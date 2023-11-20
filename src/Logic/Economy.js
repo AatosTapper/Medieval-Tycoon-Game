4f6a0c0aa7453f1d908f932851cf7a8d44bc7199
AatosTapper <aatos.tapper@icloud.com>
2023-11-20 17:14:28 +0200

    some updates to economy

--- src/Logic/Economy.js
+++ src/Logic/Economy.js
@@ -3,43 +3,59 @@ import { RandGauss } from "./RandGauss";
 // How much the delta value affects the new derivative
 // 2 = delta value is half of the next derivative
 // 3 = delta value is one third of the next derivative
-const NEW_DER_FRAC = 3;
+const NEW_DER_FRAC = 2;
 
-const CHAOS = 0.25; // Random offset range
-const INERTIA = 0.15; // How much derivative momentum is conserved
-const INERTIA_RANGE = 0.05; // Random range
+const CHAOS = 0.3; // Random offset range
+const INERTIA = 1.5; // How much derivative momentum is conserved
+const INERTIA_RANGE = 0.2; // Random range
 const MARKET_INFLUENCE = 0.05;
 const MARKET_INFLUENCE_RANGE = 0.1;
+const DEVIATION = 3; // how far from 0 can value go before market reacts
+const DEVIATION_RANGE = 1;
 
 const calcOffset = (derivative, globalValue) => {
-    const negMult = derivative < 0 ? 1.5 : 1.0; // Value goes down more than up
+    const derSquare = derivative < 0 ? -(derivative ** 2 * 2) : derivative ** 2 * 2;
     const randOffset = RandGauss(0, CHAOS);
     const derivativeMult = RandGauss(INERTIA, INERTIA_RANGE);
-    const globalMarketOffset = RandGauss(MARKET_INFLUENCE, MARKET_INFLUENCE_RANGE) * globalValue * 0.1;
-    return derivative * negMult * derivativeMult + randOffset + globalMarketOffset;
+    const globalMarketOffset = RandGauss(MARKET_INFLUENCE, MARKET_INFLUENCE_RANGE) * globalValue;
+    let output = derSquare * derivativeMult + randOffset + globalMarketOffset;
+    output = DEVIATION * Math.tanh(output / DEVIATION);
+
+    return output;
 }
 
-const calcDerivative = (oldDerivative, deltaVal) => {
-    return ((oldDerivative * NEW_DER_FRAC) + deltaVal) / (NEW_DER_FRAC + 1);
+const calcDerivative = (oldDerivative, deltaVal, value) => {
+    const randTreshold = RandGauss(DEVIATION, DEVIATION_RANGE);
+    let derivative = ((oldDerivative * NEW_DER_FRAC) + deltaVal) / (NEW_DER_FRAC + 1);
+    if ((value > randTreshold) || (value < -randTreshold)) {
+        derivative *= -1.3;
+    }
+    return derivative;
 }
 
 const updateAll = (setWorldState) => {
     setWorldState(oldState => {
         const oldGlobal = oldState.company.globalValue;
+
         const oldMine = oldState.company.mine;
         const mineOffset = calcOffset(oldMine.derivative, oldGlobal);
+        const mineValue = oldMine.value + mineOffset;
         
         const oldFarm = oldState.company.farm;
         const farmOffset = calcOffset(oldFarm.derivative, oldGlobal);
+        const farmValue = oldFarm.value + farmOffset;
 
         const oldWood = oldState.company.wood;
         const woodOffset = calcOffset(oldWood.derivative, oldGlobal);
+        const woodValue = oldWood.value + woodOffset;
 
         const oldMagic = oldState.company.magic;
         const magicOffset = calcOffset(oldMagic.derivative, oldGlobal);
+        const magicValue = oldMagic.value + magicOffset;
 
         const oldCrafting = oldState.company.crafting;
         const craftingOffset = calcOffset(oldCrafting.derivative, oldGlobal);
+        const craftingValue = oldCrafting.value + craftingOffset;
 
         const newGlobalValue = (
             oldMine.value
@@ -57,24 +73,24 @@ const updateAll = (setWorldState) => {
             company: {
                 ...oldState.company,
                 mine: {
-                    value: oldMine.value + mineOffset,
-                    derivative: calcDerivative(oldMine.derivative, mineOffset)
+                    value: mineValue,
+                    derivative: calcDerivative(oldMine.derivative, mineOffset, mineValue)
                 },
                 farm: {
-                    value: oldFarm.value + farmOffset,
-                    derivative: calcDerivative(oldFarm.derivative, farmOffset)
+                    value: farmValue,
+                    derivative: calcDerivative(oldFarm.derivative, farmOffset, farmValue)
                 },
                 wood: {
-                    value: oldWood.value + woodOffset,
-                    derivative: calcDerivative(oldWood.derivative, woodOffset)
+                    value: woodValue,
+                    derivative: calcDerivative(oldWood.derivative, woodOffset, woodValue)
                 },
                 magic: {
-                    value: oldMagic.value + magicOffset,
-                    derivative: calcDerivative(oldMagic.derivative, magicOffset)
+                    value: magicValue,
+                    derivative: calcDerivative(oldMagic.derivative, magicOffset, magicValue)
                 },
                 crafting: {
-                    value: oldCrafting.value + craftingOffset,
-                    derivative: calcDerivative(oldCrafting.derivative, craftingOffset)
+                    value: craftingValue,
+                    derivative: calcDerivative(oldCrafting.derivative, craftingOffset, craftingValue)
                 },
                 globalValue: newGlobalValue
             }
